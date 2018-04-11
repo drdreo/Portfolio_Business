c9c3e1f188fef13b6798d1b1842620b1cd6faab2
Andreas Hahn <drdreo@users.noreply.github.com>
2018-04-11 08:56:59 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app" class="container is-fluid">
+    <img width="25%" src="./assets/logo.png">
+    <HelloWorld/>
+    <Expertise/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+import Expertise from "./components/Expertise";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    Expertise
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Expertise.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="level">
+    <h3>What I can bring to your team</h3>
+
+    <div class="columns level-item is-mobile">  
+      <div class="column is-half">
+        <p>te</p>
+      </div>
+      <div class="column">
+        <h4>Webdesign & UI</h4>
+        <p>Users evaluate a design in 50 milliseconds. You need people to take your brand seriously. I will help you make those first few milliseconds count.</p>
+      </div>    
+
+      <div class="column">
+        <h4>Web development</h4>
+        <p>Bringing your idea to life with the MEAN stack. Expertise in Node.js and Angular+. Creating sustainable code base for easy future maintainance</p>
+      </div>
+
+      <div class="column">
+        <h4>Digital Strategy</h4>
+        <p>I’ll work with you to understand your business and make sure everything we do aligns with your goals.</p>
+      </div>    
+    </div>
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Expertise",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+p {
+  text-align: left;
+}
+</style>
